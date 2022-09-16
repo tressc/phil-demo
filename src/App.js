0966481aaf44c3baf70e773b7930cc908515c377
tressc <tresscd@gmail.com>
2022-09-16 15:24:54 -0400

    Add routing.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useParams,
+  useLocation,
+} from "react-router-dom";
+
+const Home = () => {
+  return <h2>Home</h2>;
+};
+
+const PageOne = () => {
+  return <h2>Welcome to page 1!</h2>;
+};
+
+const PageTwo = () => {
+  return <h2>Welcome to page 2!</h2>;
+};
+
+const RandomPage = () => {
+  let params = useParams();
+  return <h2>You manually navigated to page {params.randomInput}!</h2>;
+};
+
+const QueryExample = () => {
+  // useLocation returns a dict
+  // we're just extracting a single keyed value here
+  // same as location = useLocation(), search = location.search
+  const { search } = useLocation();
+
+  // memoize this value, only rerun if value of "search" changes
+  const query = React.useMemo(() => new URLSearchParams(search), [search]);
+
+  const fruit = query.get("fruit");
+  if (fruit) {
+    return <h4>The fruit in the query string is {fruit}.</h4>;
+  } else {
+    return <h4>There is not fruit in the query string.</h4>;
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // wraps entire application with the router
+    <Router>
+      <div>
+        {/* This area will render regardless of the path because it's outside of the <Routes> */}
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/1">Page One</Link>
+          <Link to="/2">Page Two</Link>
+        </ul>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/1" element={<PageOne />} />
+          <Route path="/2" element={<PageTwo />} />
+          <Route path="/:randomInput" element={<RandomPage />} />
+        </Routes>
+      </div>
+      <div>
+        {/* This area will render regardless of the path because it's outside of the <Routes> */}
+        <QueryExample />
+      </div>
+    </Router>
   );
 }
 
